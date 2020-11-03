fdb05cf44e08f0a8898cc52f248deb8d33093fc8
Anna Kaser <kaserann96@gmail.com>
2020-11-02 20:15:04 -0700

    server requirements

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const hbs = exphbs.create({});
+
+const SequelizeStore = require('connect-session-sequelize');
+
+const sess = {
+    secret: 'Secrets',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
