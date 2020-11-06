3f568df1f2f8e008d93066e1b790eae188b4c5c8
Logan <logandpage5@gmail.com>
2020-11-05 19:45:48 -0700

    user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,19 +4,113 @@ const {
     ToDo
 } = require('../../models')
 
-//Get all users
-router.get('/', (req,res) => {
-    User.findAll({
+//Get specific user
+router.get('/:id', (req, res) => {
+    User.findOne({
         attributes: {
             exclude: ['password']
-        }
+        },
+        where: {
+            id: req.params.id,
+        },
+        include: [{
+            model: ToDo,
+            attributes: ['id', 'title', 'contents']
+        }]
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({
+                message: 'No user found'
+            });
+            return;
+        }
+        res.json(dbUserData)
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
 
-//Get specific user
-router.get('/:id', (req, res))
+
+// Create a user
+router.post('/', (req, res) => {
+    User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+                res.json(dbUserData);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.post('/login', (req, res) => {
+    User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(400).json({
+                    message: 'No user with that email address!'
+                });
+                return;
+            }
+
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json({
+                    user: dbUserData,
+                    message: 'You are now logged in!'
+                });
+            });
+
+            const validPassword = dbUserData.checkPassword(req.body.password);
+
+            if (!validPassword) {
+                res.status(400).json({
+                    message: 'Password Incorrect'
+                });
+                return;
+            }
+
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json({
+                    user: dbUserData,
+                    message: 'You are now logged in!'
+                });
+            });
+        });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+
+});
+
+module.exports = router;
