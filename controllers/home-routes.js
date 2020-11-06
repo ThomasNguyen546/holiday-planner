be51d6bd6c89232aac5ab01bc65fa150241aaf3f
Anna Kaser <kaserann96@gmail.com>
2020-11-05 18:54:50 -0700

    get home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, ToDo } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  ToDo.findAll({
+    attributes: [
+      'id',
+      'title',
+      'contents'
+    ]
+  })
+    .then(dbToDoData => {
+      const todo = dbToDoData.map(post => post.get({ plain: true }));
+      res.render('homepage', {
+        todos,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
