72bdab4cea52a6bd8e4168f3316e391db1cd0a97
Logan <logandpage5@gmail.com>
2020-11-06 16:46:32 -0700

    todo-routes

--- controllers/api/todo-routes.js
+++ controllers/api/todo-routes.js
@@ -5,7 +5,7 @@ const {
 } = require('../../models');
 const { post } = require('../home-routes');
 
-//Get all posts
+//Get all ToDos
 router.get('/', (req, res) => {
     ToDo.findAll({
         attributes: ['id', 'title', 'contents'],
@@ -17,14 +17,66 @@ router.get('/', (req, res) => {
     });
 });
 
-//Updata a ToDo
+//Create a ToDos
+router.post('/', (req,res) => {
+    console.log("Elfs at work");
+    ToDo.create({
+        title: req.body.title,
+        // content: req.body.++++,
+        user_id: req.session.user_id
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+//Update a ToDo
 router.put('/:id', (req, res) => {
     ToDo.update({
+        title: req.body.title,
+        // content: req.body.+++
+    }, {
         where: {
             id: req.params.id,
         },
     })
     .then((dbPostData) => {
-        if(!dbPostData) {}
+        if (!dbPostData) {
+            res.status(404).json({
+                message: "No post found with this id"
+            });
+            return;
+        }
+        res.json(dbPostData);
     })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 })
+
+//Delete a post
+router.delete("/:id", (req, res) => {
+    ToDo.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((dbPostData) => {
+            if (!dbPostData) {
+                res.status(404).json({
+                    message: "The Elfs were unable to locate this post."
+                });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
