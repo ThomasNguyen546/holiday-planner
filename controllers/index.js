2cbf6fd29ff9a72f58731fb3a65dddb776d1a4f2
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-11-08 10:17:53 -0700

    pull

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 
+
 router.use('/', homeRoutes);
+
 
 router.use((req, res) => {
     res.status(404).end();
